638f2e285b50acaa4f4149662529543435e63e27
SwiftKobe <folledoswag@gmail.com>
2018-07-03 21:30:18 -0400

    - added a sign up and log in functionality with a switch for a driver or a rider when signing up

--- FolledoUber/ViewController.swift
+++ FolledoUber/ViewController.swift
@@ -7,8 +7,8 @@
 //
 
 // 1) Firebase Installation
-// 2) Login & Signup
-// 3) The Rider ViewController
+// 2) Login & Signup = Setting up login and sign up with firebase
+// 3) The Rider ViewController =
 // 4) The Driver ViewController
 // 5) Sharing the Driver's Location with the Driver
 
@@ -34,13 +34,61 @@ class ViewController: UIViewController {
 
     @IBAction func topTapped(_ sender: Any) { //2 //8mins
         
+        if emailTextField.text == "" || passwordTextField.text == "" { //2 //17mins
+            displayAlert(title: "Missing Information", message: "You must provide both an email and password") //2 //21mins
+        } else {
+            if let email = emailTextField.text { //2 //24mins
+                if let password = passwordTextField.text { //2 //24mins
+            
+            if signUpMode { //2 //22mins //SIGN UP
+                
+                Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in //2 //23mins
+                    if error != nil { //2 //24mins
+                        self.displayAlert(title: "Error", message: error!.localizedDescription) //2 //24mins
+                    } else { //2 //25mins
+                        print("Sign Up Success")
+                    }
+                }) //2 //23mins
+            } else { //2 //22mins //LOG IN
+                
+                Auth.auth().signIn(withEmail: email, password: password) { (user, error) in //2 //26mins
+                    if error != nil { //2 //26mins
+                        self.displayAlert(title: "Error", message: error!.localizedDescription) //2 //26mins
+                    } else { //2 //26mins //Dont forget to setup an authentication process
+                        print("Log In Success")
+                    }
+                    
+                }
+                
+            }}}
+        }
         
     }
     
-    @IBAction func bottomTapped(_ sender: Any) { //2 //8mins
-        
-        
+    func displayAlert(title: String, message: String) { //2 //18mins
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert) //2 //20mins
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil)) //2 //20mins
+        self.present(alertController, animated: true, completion: nil) //2 //20mins
     }
+    
+//bottomTapped method
+    @IBAction func bottomTapped(_ sender: Any) { //2 //8mins
+        if signUpMode { //2 //15mins
+            topButton.setTitle("Log In", for: .normal) //2 //15mins
+            bottomButton.setTitle("Switch to Sign Up", for: .normal) //2 //15mins
+            riderLabel.isHidden = true //2 //15mins
+            driverLabel.isHidden = true //2 //15mins
+            riderDriverSwitch.isHidden = true //2 //15mins
+            signUpMode = false //2 //15mins
+        } else { //2 //15mins
+            topButton.setTitle("Sign Up", for: .normal) //2 //15mins
+            bottomButton.setTitle("Switch to Log In", for: .normal) //2 //15mins
+            riderLabel.isHidden = false //2 //15mins
+            driverLabel.isHidden = false //2 //15mins
+            riderDriverSwitch.isHidden = false //2 //15mins
+            signUpMode = true //2 //15mins
+        } //2 //15mins
+    } //2 //15mins
     
 
 }
